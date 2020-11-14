5b167c10661d938c04d493016f93b852c9901797
hikaru ishiguro <rukahirogushii0320@gmail.com>
2020-11-14 06:47:37 +0000

    CommonComponent作成

---
+++ src/components/CommonComponent.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="common">common test</div>
+</template>
+
+<script>
+export default {
+    props: {},
+};
+</script>
+
+<style scoped>
+.common {
+    font-size: 10em;
+}
+</style>
